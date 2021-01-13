a48e8cc700d02cd76c4132da08d4192cd13c36e4
Roman Agarkov <agarkovromachka@mail.ru>
2021-01-14 01:03:34 +0300

    add brainEven logic file

---
+++ src/brain-even.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+
+export default function brainEven() {
+  const numbers = [
+    { number: 15, rightAnswer: 'no' },
+    { number: 6, rightAnswer: 'yes' },
+    { number: 7, rightAnswer: 'no' },
+  ];
+  let countOfRightAnswers = 0;
+
+  // Greeting & userName saving
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  // Show questions & counting right answers
+  for (let i = 0; i < numbers.length; i += 1) {
+    const element = numbers[i];
+    const answer = readlineSync.question(`Question: ${element.number} \nYour answer: `);
+    if (answer === element.rightAnswer) {
+      countOfRightAnswers += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${element.rightAnswer}'.\nLet's try again, ${userName}!`);
+      break;
+    }
+  }
+
+  // Show 'Congratulations' if all answers are right
+  if (countOfRightAnswers === numbers.length) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+}
