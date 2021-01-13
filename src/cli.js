7474866a01eefaf5608558cc5a7c10a1b8b3322b
Roman Agarkov <agarkovromachka@mail.ru>
2021-01-13 18:28:06 +0300

    refactor with eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
 // Wait for user's response.
-export function greeting() {
-    const userName = readlineSync.question('May I have your name? ');
-    return console.log(`Hello ${userName}!`);
+export default function greeting() {
+  const userName = readlineSync.question('May I have your name? ');
+  return console.log(`Hello ${userName}!`);
 }
-
