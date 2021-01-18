9d73bdfa2dea1b2840ae139d657e84e963f48389
Roman Agarkov <agarkovromachka@mail.ru>
2021-01-18 12:18:23 +0300

    refactor getRandomOperation

--- src/utils/getRandomOperation.js
+++ src/utils/getRandomOperation.js
@@ -1,8 +1,10 @@
+import getRandomNumberInRange from './getRandomNumberInRange.js';
+
 export default function getRandomOperation() {
   const operations = '-+*';
   const min = 1;
   const max = 3;
-  const number = Math.floor(Math.random() * (max - min + 1)) + min;
+  const number = getRandomNumberInRange(min, max);
 
   return operations.charAt(number - 1);
 }
